d9885939b449e5b66e711240f54e841cb25ca472
Raymart <raymartformalejo00@gmail.com>
2023-12-10 00:13:39 +0800

    refactor: Change allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,8 +2,6 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5175",
   "https://raymart-personal-blog.vercel.app",
-  "https://raymart-personal-blog-raymartbediaformalejo.vercel.app",
-  "https://raymart-personal-blog.vercel.app",
 ];
 
 module.exports = allowedOrigins;
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,7 +1,13 @@
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin: "https://raymart-personal-blog.vercel.app",
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };

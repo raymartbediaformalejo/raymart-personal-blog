41d9a28bcb81dd74c7c0a38d071162f91e5e4f3e
Raymart <raymartformalejo00@gmail.com>
2023-12-10 00:09:00 +0800

    feat: add allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5175",
   "https://raymart-personal-blog.vercel.app",
+  "https://raymart-personal-blog-raymartbediaformalejo.vercel.app",
+  "https://raymart-personal-blog.vercel.app",
 ];
 
 module.exports = allowedOrigins;

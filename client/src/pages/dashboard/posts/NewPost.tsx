e7070cbf7ff2704219db8eac02949b4188a215f2
Raymart <raymartformalejo00@gmail.com>
2023-12-09 23:47:03 +0800

    feat: Reset the post state when successfully added a post

--- client/src/pages/dashboard/posts/NewPost.tsx
+++ client/src/pages/dashboard/posts/NewPost.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
-import { Theme, useTheme } from "@mui/material/styles";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -14,6 +13,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
+import Container from "@mui/material/Container";
 
 import { useAppSelector } from "../../../redux/hooks/useAppSelector";
 import { selectAllCategories } from "../../../redux/categories/categories.api";
@@ -34,27 +34,26 @@ const loadQuillNoSSRWrapper = () =>
 const DynamicQuillNoSSRWrapper = React.lazy(loadQuillNoSSRWrapper);
 
 const NewPost = () => {
-  const { handleSubmit, control, watch, setValue, reset, formState } =
-    useForm<TPost>({
-      shouldFocusError: false,
-      defaultValues: {
-        author: "6572f6f8a582516575637ceb",
-        category: [],
-        tag: [],
-        title: "",
-        summary: "",
-        image: "",
-        content: "",
-        status: "",
-        visibility: "",
-        featured: false,
-        articles: [],
-      },
-      resolver: zodResolver(postSchema),
-    });
+  const { handleSubmit, control, watch, reset, formState } = useForm<TPost>({
+    shouldFocusError: false,
+    defaultValues: {
+      author: "6572f6f8a582516575637ceb",
+      category: [],
+      tag: [],
+      title: "",
+      summary: "",
+      image: "",
+      content: "",
+      status: "",
+      visibility: "",
+      featured: false,
+      articles: [],
+    },
+    resolver: zodResolver(postSchema),
+  });
 
-  // const [addNewPost, { isLoading, isSuccess, isError, error }] =
-  //   useAddNewPostMutation();
+  const [addNewPost, {  isSuccess, isError, error }] =
+    useAddNewPostMutation();
   // const { data: categories } = useGetCategoriesQuery();
   const categories = useAppSelector(selectAllCategories);
   const tags = useAppSelector(selectAllTags);
@@ -91,23 +90,12 @@ const NewPost = () => {
   // const categoriesOption = categories;
 
   const onSubmit = async (data: TPost) => {
-    const {
-      author,
-      category,
-      tag,
-      title,
-      summary,
-      image,
-      content,
-      status,
-      visibility,
-      featured,
-    } = data;
-
     try {
       console.log("========FINAL===============");
 
       console.log(watch());
+      await addNewPost({ ...data });
+      reset();
     } catch (error) {
       console.log(`💥 ${error}`);
     }
@@ -117,216 +105,228 @@ const NewPost = () => {
     // }
   };
   console.log("ERRORS: ", formState.errors);
+  if (isError) console.log(error);
+  if (isSuccess) console.log("Successfully add post");
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          name="category"
-          defaultValue={[]}
-          render={({ field }) => (
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel id="demo-multiple-name-label">Category</InputLabel>
-              <Select
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            marginTop: "1rem",
+          }}
+        >
+          <Controller
+            control={control}
+            name="category"
+            defaultValue={[]}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel id="category">Category</InputLabel>
+                <Select
+                  {...field}
+                  labelId="category"
+                  id="category"
+                  multiple
+                  value={field.value}
+                  input={<OutlinedInput label="Name" />}
+                >
+                  {categoryOptions.map((categ) => (
+                    <MenuItem key={categ.label} value={categ.value}>
+                      {categ.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
+          <Controller
+            control={control}
+            name="tag"
+            defaultValue={[]}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel id="tag">Tag</InputLabel>
+                <Select
+                  {...field}
+                  labelId="tag"
+                  id="tag"
+                  multiple
+                  value={field.value}
+                  input={<OutlinedInput label="Name" />}
+                >
+                  {tagOptions.map((tag) => (
+                    <MenuItem key={tag.label} value={tag.value}>
+                      {tag.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
+          <Controller
+            control={control}
+            name="title"
+            render={({ field }) => (
+              <TextField
                 {...field}
-                labelId="demo-multiple-name-label"
-                id="demo-multiple-name"
-                multiple
-                value={field.value}
-                input={<OutlinedInput label="Name" />}
-                // MenuProps={MenuProps}
-              >
-                {categoryOptions.map((categ) => (
-                  <MenuItem key={categ.label} value={categ.value}>
-                    {categ.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-        />
-        <Controller
-          control={control}
-          name="tag"
-          defaultValue={[]}
-          render={({ field }) => (
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel id="demo-multiple-name-label">Tag</InputLabel>
-              <Select
-                {...field}
-                labelId="demo-multiple-name-label"
-                id="demo-multiple-name"
-                multiple
-                value={field.value}
-                input={<OutlinedInput label="Name" />}
-                // MenuProps={MenuProps}
-              >
-                {tagOptions.map((tag) => (
-                  <MenuItem key={tag.label} value={tag.value}>
-                    {tag.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-        />
-        <Controller
-          control={control}
-          name="title"
-          render={({ field }) => (
-            <TextField
-              {...field}
-              id="outlined-basic"
-              label="Title"
-              variant="outlined"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="summary"
-          render={({ field }) => (
-            <TextField
-              {...field}
-              id="outlined-basic"
-              label="Summary"
-              variant="outlined"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="image"
-          render={({ field }) => {
-            return (
-              <Button
+                id="title"
+                label="Title"
                 variant="outlined"
-                startIcon={<UploadFileIcon />}
-                sx={{ marginRight: "1rem" }}
-              >
-                Upload Cover Photo
-                <input
-                  type="file"
-                  accept=".csv"
-                  hidden
-                  onChange={async (image) => {
-                    const fileInput = image.target as HTMLInputElement;
+                autoComplete="off"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="summary"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                id="summary"
+                label="Summary"
+                variant="outlined"
+                autoComplete="off"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="image"
+            render={({ field }) => {
+              return (
+                <Button
+                  component="label"
+                  variant="outlined"
+                  startIcon={<UploadFileIcon />}
+                >
+                  Upload Cover Photo
+                  <input
+                    type="file"
+                    accept="image/*"
+                    hidden
+                    onChange={async (image) => {
+                      const fileInput = image.target as HTMLInputElement;
 
-                    if (fileInput.files && fileInput.files.length > 0) {
-                      const reader = new FileReader();
-                      reader.readAsDataURL(fileInput.files[0]);
+                      if (fileInput.files && fileInput.files.length > 0) {
+                        const reader = new FileReader();
+                        reader.readAsDataURL(fileInput.files[0]);
 
-                      for (const file of fileInput.files) {
-                        console.log("file: ", URL.createObjectURL(file));
-                        formData.append("file", file);
-                      }
-
-                      formData.append(
-                        "upload_preset",
-                        import.meta.env.VITE_CLOUDINARY_PRESET_NAME
-                      );
-
-                      try {
-                        const response = await axios.post(
-                          import.meta.env.VITE_CLOUDINARY_URL,
-                          formData
-                        );
-                        const imageData = response.data;
-
-                        console.log("Image data:", imageData.secure_url);
-                        if (imageData.secure_url) {
-                          field.onChange(imageData.secure_url);
+                        for (const file of fileInput.files) {
+                          console.log("file: ", URL.createObjectURL(file));
+                          formData.append("file", file);
                         }
-                      } catch (error) {
-                        console.error("Error uploading image:", error);
+
+                        formData.append(
+                          "upload_preset",
+                          import.meta.env.VITE_CLOUDINARY_PRESET_NAME
+                        );
+
+                        try {
+                          const response = await axios.post(
+                            import.meta.env.VITE_CLOUDINARY_URL,
+                            formData
+                          );
+                          const imageData = response.data;
+
+                          console.log("Image data:", imageData.secure_url);
+                          if (imageData.secure_url) {
+                            field.onChange(imageData.secure_url);
+                          }
+                        } catch (error) {
+                          console.error("Error uploading image:", error);
+                        }
                       }
-                    }
+                    }}
+                  />
+                </Button>
+              );
+            }}
+          />
+          {watch("image") && <img src={watch("image")} alt="cover photo" />}
+          <Controller
+            name="featured"
+            control={control}
+            render={({ field }) => (
+              <FormControlLabel
+                {...field}
+                control={<Checkbox defaultChecked />}
+                label="Is featured?"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="status"
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel id="status">Status</InputLabel>
+                <Select
+                  {...field}
+                  labelId="status"
+                  id="status"
+                  value={field.value}
+                  input={<OutlinedInput label="Name" />}
+                  // MenuProps={MenuProps}
+                >
+                  {statusOptions.map((status) => (
+                    <MenuItem key={status.label} value={status.value}>
+                      {status.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />{" "}
+          <Controller
+            control={control}
+            name="visibility"
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel id="visibility">Visibility</InputLabel>
+                <Select
+                  {...field}
+                  labelId="visibility"
+                  id="visibility"
+                  value={field.value}
+                  input={<OutlinedInput label="Visibility" />}
+                  // MenuProps={MenuProps}
+                >
+                  {visibilityOptions.map((visibility) => (
+                    <MenuItem key={visibility.label} value={visibility.value}>
+                      {visibility.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
+          <Controller
+            name="content"
+            control={control}
+            render={({ field }) => (
+              <Suspense fallback={<div>Loading Quill editor</div>}>
+                <DynamicQuillNoSSRWrapper
+                  id="content"
+                  className={classes["editor"]}
+                  theme="snow"
+                  modules={QUILL_EDITOR_MODULES}
+                  formats={QUILL_EDITOR_FORMATS}
+                  value={field.value || ""}
+                  onChange={(text) => {
+                    field.onChange(text);
                   }}
                 />
-              </Button>
-            );
-          }}
-        />
-        <Controller
-          name="featured"
-          control={control}
-          render={({ field }) => (
-            <FormControlLabel
-              {...field}
-              control={<Checkbox defaultChecked />}
-              label="Label"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="status"
-          render={({ field }) => (
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel id="demo-multiple-name-label">Status</InputLabel>
-              <Select
-                {...field}
-                labelId="demo-multiple-name-label"
-                id="demo-multiple-name"
-                value={field.value}
-                input={<OutlinedInput label="Name" />}
-                // MenuProps={MenuProps}
-              >
-                {statusOptions.map((status) => (
-                  <MenuItem key={status.label} value={status.value}>
-                    {status.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-        />{" "}
-        <Controller
-          control={control}
-          name="visibility"
-          render={({ field }) => (
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel id="demo-multiple-name-label">Category</InputLabel>
-              <Select
-                {...field}
-                labelId="demo-multiple-name-label"
-                id="demo-multiple-name"
-                value={field.value}
-                input={<OutlinedInput label="Name" />}
-                // MenuProps={MenuProps}
-              >
-                {visibilityOptions.map((visibility) => (
-                  <MenuItem key={visibility.label} value={visibility.value}>
-                    {visibility.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          )}
-        />
-        <Controller
-          name="content"
-          control={control}
-          render={({ field }) => (
-            <Suspense fallback={<div>Loading Quill editor</div>}>
-              <DynamicQuillNoSSRWrapper
-                id="content"
-                className={classes["editor"]}
-                theme="snow"
-                modules={QUILL_EDITOR_MODULES}
-                formats={QUILL_EDITOR_FORMATS}
-                value={field.value || ""}
-                onChange={(text) => {
-                  field.onChange(text);
-                }}
-              />
-            </Suspense>
-          )}
-        />
-        <Button type="submit" variant="contained">
-          Save
-        </Button>
+              </Suspense>
+            )}
+          />
+          <Button type="submit" variant="contained">
+            Save
+          </Button>
+        </Container>
       </form>
     </div>
   );

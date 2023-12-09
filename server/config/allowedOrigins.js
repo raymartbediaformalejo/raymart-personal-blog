b7be19f1004b5f6a73ea0a6030244678f3aa309d
Raymart <raymartformalejo00@gmail.com>
2023-12-10 00:03:17 +0800

    feat(server): Include the deployed client to allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5175"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5175",
+  "https://raymart-personal-blog.vercel.app",
+];
 
 module.exports = allowedOrigins;

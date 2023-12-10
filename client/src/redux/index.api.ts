098c5c5ecc2fac1891f381fedf5b8f5f52ca9f6f
Raymart <raymartformalejo00@gmail.com>
2023-12-10 17:54:30 +0800

    refactor: Change the baseURL

--- client/src/redux/index.api.ts
+++ client/src/redux/index.api.ts
@@ -8,9 +8,9 @@ import type {
 import { RootState } from "./index";
 
 // baseUrl: "http://localhost:5500",
-// baseUrl: import.meta.env.VITE_API_URL,
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5500",
+  // baseUrl: "http://localhost:5500",
+  baseUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;

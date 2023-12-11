f51426b9df18085dbffb748fb55949fb0da26995
Raymart <raymartformalejo00@gmail.com>
2023-12-11 21:59:31 +0800

    feat: Implement code syntax highlighter

--- client/src/pages/articles/SingleArticle.tsx
+++ client/src/pages/articles/SingleArticle.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import { useParams } from "react-router-dom";
+import { vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 import { useAppSelector } from "../../redux/hooks/useAppSelector";
 import { selectPostById } from "../../redux/posts/posts.api";
 
@@ -7,16 +9,71 @@ const SingleArticle = () => {
   const { id } = useParams();
   const post = useAppSelector((state) => selectPostById(state, id as string));
 
-  console.log("POST: ", post);
+  const splitStringByPreTag = (inputString: string) => {
+    const preTagStart = "<pre";
+    const preTagEnd = "</pre>";
+    const resultArray = [];
+
+    let startIndex = 0;
+    while (startIndex < inputString.length) {
+      const preTagStartIndex = inputString.indexOf(preTagStart, startIndex);
+      if (preTagStartIndex === -1) {
+        // No more occurrences of "<pre" found, add the remaining part
+        resultArray.push(inputString.substring(startIndex));
+        break;
+      }
+
+      const preTagEndIndex = inputString.indexOf(preTagEnd, preTagStartIndex);
+      if (preTagEndIndex === -1) {
+        // No corresponding "</pre>" found, add the remaining part
+        resultArray.push(inputString.substring(startIndex));
+        break;
+      }
+
+      // Add the part before <pre
+      resultArray.push(inputString.substring(startIndex, preTagStartIndex));
+      // Add the <pre...> content
+      resultArray.push(
+        inputString.substring(
+          preTagStartIndex,
+          preTagEndIndex + preTagEnd.length
+        )
+      );
+
+      startIndex = preTagEndIndex + preTagEnd.length;
+    }
+
+    return resultArray;
+  };
+
+  const removePreTags = (inputString: string) => {
+    const regex = /<pre class="ql-syntax" spellcheck="false">|<\/pre>/g;
+    return inputString.replace(regex, "");
+  };
 
   return (
     <div>
       <img src={post?.image} />
       <h2>{post?.title}</h2>
       <code>Hello world</code>
-      {post?.content && (
-        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
-      )}
+      {post &&
+        post?.content &&
+        splitStringByPreTag(post.content).map((content) => {
+          if (content.startsWith("<pre")) {
+            return (
+              <SyntaxHighlighter key={content} language="jsx" style={vs2015}>
+                {`${removePreTags(content)}`}
+              </SyntaxHighlighter>
+            );
+          } else {
+            return (
+              <div
+                key={content}
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></div>
+            );
+          }
+        })}
     </div>
   );
 };
